1406b18e83dd1b8cb1d0a16f59551f03f18302d1
Paul Mars <paul.mars@canonical.com>
2025-12-01 13:33:07 +0100

    feat: refine first approach
    Signed-off-by: Paul Mars <paul.mars@canonical.com>

--- internal/manifestutil/manifestutil.go
+++ internal/manifestutil/manifestutil.go
@@ -388,27 +388,27 @@ func RootFSManifest(release *setup.Release, targetDir string) (*manifest.Manifes
 		return nil, err
 	}
 
-	rootfsReport, err := reportFromRootfs(targetDir)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Printf("Report: %#v\n", rootfsReport)
-
-	manifestReport, err := reportFromManifest(refManifest)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Printf("Report from manifest: %#v\n", manifestReport)
-
-	err = validateReportEntries(manifestReport, rootfsReport)
-	if err != nil {
-		return nil, err
-	}
-
-	// err = validateRootfs(refManifest, targetDir)
+	// rootfsReport, err := reportFromRootfs(targetDir)
 	// if err != nil {
 	// 	return nil, err
 	// }
+	// fmt.Printf("Report: %#v\n", rootfsReport)
+
+	// manifestReport, err := reportFromManifest(refManifest)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// fmt.Printf("Report from manifest: %#v\n", manifestReport)
+
+	// err = validateReportEntries(manifestReport, rootfsReport)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	err = validateRootfs(refManifest, targetDir)
+	if err != nil {
+		return nil, err
+	}
 	return refManifest, nil
 }
 
@@ -511,97 +511,57 @@ func SliceKeys(m *manifest.Manifest) []setup.SliceKey {
 // the manifest.
 // This function works under the assumption the manifest was previously
 // validated.
+// Files not managed by chisel are not checked.
 func validateRootfs(m *manifest.Manifest, rootDir string) error {
-	hardLinkGroups, err := groupHardlinks(m)
+	pathGroups, err := groupManifestPaths(m)
 	if err != nil {
 		return err
 	}
 
-	return m.IteratePaths("", func(path *manifest.Path) error {
-		p := filepath.Join(rootDir, path.Path)
-		info, err := os.Lstat(p)
+	for _, group := range pathGroups {
+		err := verifyGroup(rootDir, group)
 		if err != nil {
 			return err
 		}
-		mode := info.Mode()
-		if fmt.Sprintf("0%o", unixPerm(mode)) != path.Mode {
-			return fmt.Errorf("tampered content: %q mode mismatch: %s recorded, %s observed", path.Path, path.Mode, mode.String())
-		}
+	}
 
-		// Verify directories
-		// TODO check sha and final_sha and size
-		if strings.HasSuffix(path.Path, "/") {
-			if !info.IsDir() {
-				return fmt.Errorf("tampered content: %q expected to be a directory", path.Path)
-			}
-			return nil
-		}
-
-		// Verify symlinks
-		if len(path.Link) > 0 {
-			link, err := os.Readlink(p)
-			if err != nil {
-				return err
-			}
-
-			if link != path.Link {
-				return fmt.Errorf("tampered content: %q link destination mismatch: %s recorded, %s observed", path.Path, path.Link, link)
-			}
-			return nil
-		}
-
-		if !mode.IsRegular() {
-			return fmt.Errorf("tampered content: unrecognized type of file.")
-		}
-
-		// Verify hardlinks
-		if path.Inode != 0 {
-			paths, ok := hardLinkGroups[path.Inode]
-			if !ok {
-				return fmt.Errorf("cannot find paths associated to this inode: %d", path.Inode)
-			}
-
-			stat, ok := info.Sys().(*syscall.Stat_t)
-			if !ok {
-				return fmt.Errorf("cannot get syscall stat info for %q", path.Path)
-			}
-			nLink := stat.Nlink
-
-			if int(nLink) != len(paths) {
-				// Working under the assumption no hardlink not managed by chisel
-				// and pointing at a chisel-managed file was added.
-				return fmt.Errorf("tampered content: %q hardlinks count mismatch: %d recorded, %d observed", path.Path, len(paths), nLink)
-			}
-		}
-
-		// Common verification for regular files
-		h, err := hash(p)
-		if err != nil {
-			return err
-		}
-		hString := hex.EncodeToString(h)
-		if len(path.FinalSHA256) > 0 {
-			if hString != path.FinalSHA256 {
-				return fmt.Errorf("tampered file %q, hash mismatch: %s recorded, %s observed", path.Path, path.FinalSHA256, hString)
-			}
-		} else if len(path.SHA256) > 0 && hString != path.SHA256 {
-			// This is effectively skipping the manifest.wall since no hash is declared on it
-			return fmt.Errorf("tampered file %q, sha256 mismatch: %s recorded, %s observed", path.Path, path.SHA256, hString)
-		}
-		return nil
-	})
+	return nil
 }
 
-// groupHardlinks groups hardlink paths by inode.
-func groupHardlinks(m *manifest.Manifest) (map[uint64][]*manifest.Path, error) {
-	hardLinkGroups := make(map[uint64][]*manifest.Path)
+type pathGroup struct {
+	headEntry *manifest.Path
+	paths     []string
+}
+
+// groupManifestPaths groups paths by inode.
+func groupManifestPaths(m *manifest.Manifest) (map[string]*pathGroup, error) {
+	pathGroups := make(map[string]*pathGroup)
+	inodeGroups := make(map[uint64]string)
 
 	err := m.IteratePaths("", func(path *manifest.Path) error {
 		inode := path.Inode
 		if inode == 0 {
+			// New single path
+			pathGroups[path.Path] = &pathGroup{
+				headEntry: path,
+				paths:     []string{path.Path},
+			}
 			return nil
 		}
-		hardLinkGroups[inode] = append(hardLinkGroups[inode], path)
+
+		groupKey, ok := inodeGroups[inode]
+		if !ok {
+			// New group of hardlink
+			pathGroups[path.Path] = &pathGroup{
+				headEntry: path,
+				paths:     []string{path.Path},
+			}
+			inodeGroups[inode] = path.Path
+			return nil
+		}
+		// Add path to the existing group
+		group := pathGroups[groupKey]
+		group.paths = append(group.paths, path.Path)
 
 		return nil
 	})
@@ -609,10 +569,128 @@ func groupHardlinks(m *manifest.Manifest) (map[uint64][]*manifest.Path, error) {
 		return nil, err
 	}
 
-	return hardLinkGroups, nil
+	// sort paths
+	for _, group := range pathGroups {
+		slices.Sort(group.paths)
+	}
+
+	return pathGroups, nil
+}
+
+// verifyGroup verifies a group of paths
+func verifyGroup(rootDir string, group *pathGroup) error {
+	path := group.headEntry
+
+	info, err := verifyPath(rootDir, path)
+	if err != nil {
+		return err
+	}
+
+	// Verify hardlinks
+	if len(group.paths) == 0 {
+		// Not a hardlink, nothing more to do
+		return nil
+	}
+
+	headInode, err := getPhysicalInode(info)
+	if err != nil {
+		return err
+	}
+
+	return verifyHardlinks(headInode, rootDir, path.Path, group.paths)
+}
+
+func verifyPath(rootDir string, path *manifest.Path) (os.FileInfo, error) {
+	p := filepath.Join(rootDir, path.Path)
+	info, err := os.Lstat(p)
+	if err != nil {
+		return nil, err
+	}
+	mode := info.Mode()
+	if fmt.Sprintf("0%o", unixPerm(mode)) != path.Mode {
+		return nil, fmt.Errorf("tampered content: %q mode mismatch: %s recorded, %s observed", path.Path, path.Mode, mode.String())
+	}
+
+	// Verify directories
+	if strings.HasSuffix(path.Path, "/") {
+		if !info.IsDir() {
+			return nil, fmt.Errorf("tampered content: %q expected to be a directory", path.Path)
+		}
+		return info, nil
+	}
+
+	// Verify symlinks
+	if len(path.Link) > 0 {
+		link, err := os.Readlink(p)
+		if err != nil {
+			return nil, err
+		}
+
+		if link != path.Link {
+			return nil, fmt.Errorf("tampered content: %q link destination mismatch: %s recorded, %s observed", path.Path, path.Link, link)
+		}
+		return info, nil
+	}
+
+	if !mode.IsRegular() {
+		return nil, fmt.Errorf("tampered content: unrecognized type of file.")
+	}
+
+	// Verify size
+	if info.Size() != int64(path.Size) {
+		return nil, fmt.Errorf("tampered file: %q, size mimsmatch: %d recorded, %d observed", path.Path, info.Size(), int64(path.Size))
+	}
+
+	// Common verification for regular files
+	// Most expensive operation, so do it at the end.
+	h, err := hash(p)
+	if err != nil {
+		return nil, err
+	}
+	hString := hex.EncodeToString(h)
+	if len(path.FinalSHA256) > 0 {
+		if hString != path.FinalSHA256 {
+			return nil, fmt.Errorf("tampered file %q, hash mismatch: %s recorded, %s observed", path.Path, path.FinalSHA256, hString)
+		}
+	} else if len(path.SHA256) > 0 && hString != path.SHA256 {
+		// This is effectively skipping the manifest.wall since no hash is declared on it
+		return nil, fmt.Errorf("tampered file %q, sha256 mismatch: %s recorded, %s observed", path.Path, path.SHA256, hString)
+	}
+	return info, nil
+}
+
+func verifyHardlinks(headInode uint64, rootDir string, headPath string, paths []string) error {
+	for _, siblingPath := range paths[1:] {
+		siblingFullPath := filepath.Join(rootDir, siblingPath)
+
+		sibFi, err := os.Lstat(siblingFullPath)
+		if err != nil {
+			return err
+		}
+
+		// Verify Inode Equality
+		sibInode, err := getPhysicalInode(sibFi)
+		if err != nil {
+			return err
+		}
+
+		if sibInode != headInode {
+			return fmt.Errorf("tampered content: broken hardlink: %s and %s should share inode but do not", headPath, siblingPath)
+		}
+	}
+	return nil
+}
+
+func getPhysicalInode(info os.FileInfo) (uint64, error) {
+	stat, ok := info.Sys().(*syscall.Stat_t)
+	if !ok {
+		return 0, fmt.Errorf("cannot get syscall stat info for %q", info.Name())
+	}
+	return stat.Ino, nil
 }
 
 // reportFromRootfs builds a Report from root directory
+// Implementation heavily borrowed from testutil.TreeDump
 func reportFromRootfs(rootDir string) (*Report, error) {
 	report, err := NewReport(rootDir)
 	if err != nil {
