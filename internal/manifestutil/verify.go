70b2c5ec9ebf16355222f3914e53532b1a418b48
Paul Mars <paul.mars@canonical.com>
2025-12-02 11:26:47 +0100

    refactor: rework verify* funcs
    Signed-off-by: Paul Mars <paul.mars@canonical.com>

--- internal/manifestutil/verify.go
+++ internal/manifestutil/verify.go
@@ -86,98 +86,104 @@ func groupManifestPaths(m *manifest.Manifest) (map[string]*pathGroup, error) {
 func verifyGroup(rootDir string, group *pathGroup) error {
 	path := group.headEntry
 
-	info, err := verifyPath(rootDir, path)
+	fpath := filepath.Join(rootDir, path.Path)
+	info, err := os.Lstat(fpath)
 	if err != nil {
 		return err
 	}
 
-	// Verify hardlinks
-	if len(group.paths) == 0 {
-		// Not a hardlink, nothing more to do
-		return nil
-	}
-
-	headInode, err := getPhysicalInode(info)
+	err = verifyPath(info, fpath, path)
 	if err != nil {
 		return err
 	}
 
-	return verifyHardlinks(headInode, rootDir, path.Path, group.paths)
+	return verifyHardlinks(info, path.Path, rootDir, group.paths)
 }
 
-func verifyPath(rootDir string, path *manifest.Path) (os.FileInfo, error) {
-	p := filepath.Join(rootDir, path.Path)
-	info, err := os.Lstat(p)
-	if err != nil {
-		return nil, err
-	}
+// verifyPath verifies a single path against its manifest entry
+func verifyPath(info os.FileInfo, fpath string, path *manifest.Path) error {
 	mode := info.Mode()
+
+	// Verify mode
 	if fmt.Sprintf("0%o", unixPerm(mode)) != path.Mode {
-		return nil, fmt.Errorf("tampered content: %q mode mismatch: %s recorded, %s observed", path.Path, path.Mode, mode.String())
+		return fmt.Errorf("tampered content: %q mode mismatch: %s recorded, %s observed", path.Path, path.Mode, mode.String())
 	}
 
 	// Verify directories
 	if strings.HasSuffix(path.Path, "/") {
 		if !info.IsDir() {
-			return nil, fmt.Errorf("tampered content: %q expected to be a directory", path.Path)
+			return fmt.Errorf("tampered content: %q expected to be a directory", path.Path)
 		}
-		return info, nil
+		return nil
 	}
 
 	// Verify symlinks
 	if len(path.Link) > 0 {
-		link, err := os.Readlink(p)
+		link, err := os.Readlink(fpath)
 		if err != nil {
-			return nil, err
+			return err
 		}
 
 		if link != path.Link {
-			return nil, fmt.Errorf("tampered content: %q link destination mismatch: %s recorded, %s observed", path.Path, path.Link, link)
+			return fmt.Errorf("tampered content: %q link destination mismatch: %s recorded, %s observed", path.Path, path.Link, link)
 		}
-		return info, nil
+		return nil
 	}
 
 	if !mode.IsRegular() {
-		return nil, fmt.Errorf("tampered content: unrecognized type of file.")
+		return fmt.Errorf("tampered content: unrecognized type of file.")
 	}
 
 	// Verify size
 	if info.Size() != int64(path.Size) {
-		return nil, fmt.Errorf("tampered file: %q, size mimsmatch: %d recorded, %d observed", path.Path, info.Size(), int64(path.Size))
+		return fmt.Errorf("tampered file: %q, size mimsmatch: %d recorded, %d observed", path.Path, info.Size(), int64(path.Size))
 	}
 
 	// Verify hash
 	// Most expensive operation, so do it at the end.
-	h, err := hash(p)
+	h, err := hash(fpath)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	hString := hex.EncodeToString(h)
 	if len(path.FinalSHA256) > 0 {
 		if hString != path.FinalSHA256 {
-			return nil, fmt.Errorf("tampered file %q, hash mismatch: %s recorded, %s observed", path.Path, path.FinalSHA256, hString)
+			return fmt.Errorf("tampered file %q, hash mismatch: %s recorded, %s observed", path.Path, path.FinalSHA256, hString)
 		}
 	} else if len(path.SHA256) > 0 && hString != path.SHA256 {
 		// This is effectively skipping the manifest.wall since no hash is declared on it
-		return nil, fmt.Errorf("tampered file %q, sha256 mismatch: %s recorded, %s observed", path.Path, path.SHA256, hString)
+		return fmt.Errorf("tampered file %q, sha256 mismatch: %s recorded, %s observed", path.Path, path.SHA256, hString)
 	}
-	return info, nil
+	return nil
 }
 
-func verifyHardlinks(headInode uint64, rootDir string, headPath string, paths []string) error {
-	for _, siblingPath := range paths[1:] {
+func verifyHardlinks(headInfo os.FileInfo, headPath string, rootDir string, paths []string) error {
+	if len(paths) == 0 {
+		// No hardlinks, nothing to do
+		return nil
+	}
+
+	headInode, err := getPhysicalInode(headInfo)
+	if err != nil {
+		return err
+	}
+
+	for _, siblingPath := range paths {
+		if siblingPath == headPath {
+			continue
+		}
 		siblingFullPath := filepath.Join(rootDir, siblingPath)
 		sibFi, err := os.Lstat(siblingFullPath)
 		if err != nil {
 			return err
 		}
 
-		// Verify Inode Equality
 		sibInode, err := getPhysicalInode(sibFi)
 		if err != nil {
 			return err
 		}
 
+		// Verify Inode Equality
 		if sibInode != headInode {
 			return fmt.Errorf("tampered content: broken hardlink: %s and %s should share inode but do not", headPath, siblingPath)
 		}
