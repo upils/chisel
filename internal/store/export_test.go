102249da168b8b6951f75d6f02689de5949521ff
Paul Mars <paul.mars@canonical.com>
2026-06-19 16:13:49 +0200

    feat: fetch from the bin store

---
+++ internal/store/export_test.go
@@ -0,0 +1,19 @@
+package store
+
+import "net/http"
+
+var (
+	ValidateDownloadURL = validateDownloadURL
+	BinStagingEnvVar    = binStagingEnvVar
+)
+
+func FakeDo(do func(req *http.Request) (*http.Response, error)) (restore func()) {
+	_httpDo := httpDo
+	_bulkDo := bulkDo
+	httpDo = do
+	bulkDo = do
+	return func() {
+		httpDo = _httpDo
+		bulkDo = _bulkDo
+	}
+}
